6fe16615b293c9910705798fbe5f5ff67e5bcda7
neelarani <neela9624@gmail.com>
2026-01-03 16:55:57 +0600

    feat: completed  Function Composition problem

--- index.js
+++ index.js
@@ -42,3 +42,24 @@ console.log(getBookTitles(books));
 
 -Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
  */
+
+const square = num => {
+  return num * num;
+};
+
+const double = num => {
+  return num * 2;
+};
+
+const addFive = num => {
+  return num + 5;
+};
+
+const composedFunction = num => {
+  const squared = square(num);
+  const doubled = double(squared);
+  const result = addFive(doubled);
+
+  return result;
+};
+console.log(composedFunction(2));
